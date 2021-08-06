45d90d065315397f5cf8afad14868febb2ba1960
Miura Kotaro <miura-c0nt@k-pmpstudy.com>
2021-08-07 01:08:48 +0900

    DiffContent取得をストリーム化

--- src/main/kotlin/com/k/pmpstudy/OpenedFilesDiffAction.kt
+++ src/main/kotlin/com/k/pmpstudy/OpenedFilesDiffAction.kt
@@ -14,27 +14,35 @@ import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.annotations.NotNull
 
 class OpenedFilesDiffAction : AnAction() {
-    private lateinit var fileEditorManagerEx: FileEditorManagerEx
-
     override fun update(e: AnActionEvent) {
         val project = e.project
         e.presentation.isEnabledAndVisible = (project != null)
-        fileEditorManagerEx = FileEditorManagerEx.getInstanceEx(project!!)
     }
 
     override fun actionPerformed(@NotNull e: AnActionEvent) {
-//        val data: Array<VirtualFile> = fileEditorManagerEx.currentWindow.files
+        val fileEditorManagerEx: FileEditorManagerEx = FileEditorManagerEx.getInstanceEx(e.project!!)
         val data: Array<EditorWindow> = fileEditorManagerEx.splitters.windows
-        val file0 = data[0].selectedFile
-        val file1 = data[1].selectedFile
-        val cont1: DiffContent = DiffContentFactory.getInstance().create(e.project, file0)
-        val cont2: DiffContent = DiffContentFactory.getInstance().create(e.project, file1)
-        val request = SimpleDiffRequest("Opened Files Diff", cont1, cont2, "Cont1", "Cont2")
-        DiffManager.getInstance().showDiff(e.project, request)
-//        val data = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY) as Array<VirtualFile>
-//        Messages.showMessageDialog(data.joinToString("\n"), "Error", Messages.getInformationIcon()) // 1ファイルだけ表示
-//        Messages.showMessageDialog(data[1].toString(), "Error", Messages.getInformationIcon()) // 何も表示されない
-//        Messages.showMessageDialog(data.size.toString(), "Error", Messages.getInformationIcon()) // 1
-//        Messages.showMessageDialog(file0.toString() + "\n" + file1.toString(), "Error", Messages.getInformationIcon()) // 1ファイルだけ表示
+
+        if (data.size <= 1) {
+            Messages.showMessageDialog(
+                "At least 2 windows is needed.",
+                "Error",
+                Messages.getInformationIcon()
+            )
+        } else {
+            val contentList: List<DiffContent> =
+                data.map { it.selectedFile }
+                    .map { DiffContentFactory.getInstance().create(e.project, it) }
+                    .take(2)
+                    .toList()
+            val request = SimpleDiffRequest(
+                "Opened Files Diff",
+                contentList[0],
+                contentList[1],
+                "Cont1",
+                "Cont2"
+            )
+            DiffManager.getInstance().showDiff(e.project, request)
+        }
     }
 }
